20d33500be470bb77e1cac165c0d934f84ecebb6
GIles Roadnight <giles.roadnight@gmail.com>
2021-05-09 14:04:54 +0100

    Add answered questions div

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
+import { Question, SavedData } from '../../contracts';
+import { SavedDataService } from '../../services';
 
 @Component({
     selector: 'app-spinner',
@@ -8,9 +10,25 @@ import { Router } from '@angular/router';
     styleUrls: ['./spinner.component.scss'],
 })
 export class SpinnerComponent {
-    constructor(private router: Router) {}
+    private _savedData: SavedData;
+
+    constructor(private router: Router, saveDataService: SavedDataService) {
+        this._savedData = saveDataService.loadData();
+
+        this._answeredQuestions = this._savedData.questions.filter((question) => question.answered);
+    }
 
     public readonly faCog = faCog;
+
+    private _answeredQuestions: Question[];
+
+    public get answeredQuestions(): Question[] {
+        return this._answeredQuestions.concat();
+    }
+
+    public get hasQuestions(): boolean {
+        return this._savedData.questions.length > 0;
+    }
 
     public showSettings(): void {
         this.router.navigate(['settings']);
